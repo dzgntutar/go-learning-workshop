2b474181ceb9786690b5a005cb7ffe758faf029c
dutarrr <duzguntutar@gmail.com>
2021-09-13 21:17:52 +0300

    variables decleration

---
+++ 02-variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name, surname string = "Düzgün", "Tutar"
+	language := "English"
+	var age int = 34
+	var isMarried bool = true
+
+	fmt.Println(name)
+	fmt.Println(surname)
+	fmt.Println(language)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
